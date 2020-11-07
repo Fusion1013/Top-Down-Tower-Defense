b6fcb23660846ff8743e9b6436888a287f77afba
BuildTools <unconfigured@null.spigotmc.org>
2020-11-08 00:56:24 +0100

    Add StatModifierController

---
+++ Assets/Scriptable Objects/StatModifierScriptableObject.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Stat Modifier", menuName = "ScriptableObjects/StatModifier", order = 2)]
+public class StatModifierScriptableObject : ScriptableObject
+{
+    public string name;
+}
