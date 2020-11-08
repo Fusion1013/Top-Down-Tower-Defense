e7e6abea6f543f42ae65d0d91063059ea14ad1f4
William Reutmer <william.reutmer@gmail.com>
2020-11-08 23:16:22 +0100

    remove unused code in MachineModifierSO

--- Assets/Scriptable Objects/MachineModifierSO.cs
+++ Assets/Scriptable Objects/MachineModifierSO.cs
@@ -5,11 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Machine Modifier", menuName = "ScriptableObjects/MachineModifier", order = 1)]
 public class SimpleMachineModifierSO : ScriptableObject
 {
-    [Header("Machine Settings")]
-    public MachineType machineType;
-    public MovementType movementType;
-    public ShooterType shooterType;
-
     [Space]
     [Header("General Statistics")]
     public float maxHealth;
@@ -20,43 +15,4 @@ public class SimpleMachineModifierSO : ScriptableObject
     public float viewRadius;
     [Range(0, 360)]
     public float viewAngle;
-
-    [Space]
-    [Header("Shooter Settings")]
-    public float fireCooldown;
-    public LayerMask targetLayer;
-
-    [Space]
-    [Header("Reference Objects")]
-    public GameObject missile;
-    public GameObject particleOnDeath;
-
-    [ContextMenu("Choose Random Values")]
-    private void ChooseRandomValues()
-    {
-        maxHealth = Random.Range(10, 1000);
-        maxSpeed = Random.Range(1f, 20f);
-        fireCooldown = Random.Range(0.1f, 4);
-    }
-
-    public enum MachineType
-    {
-        Enemy,
-        Turret,
-        Building
-    }
-
-    public enum ShooterType
-    {
-        None,
-        Single,
-        Tripple
-    }
-
-    public enum MovementType
-    {
-        None,
-        Rotation,
-        BasicEnemy
-    }
 }
